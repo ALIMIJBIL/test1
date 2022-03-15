001199c06efee9a907ef8b41912c98bf4b2d2c98
ALIMIJBIL <alimijbilalmimar@gmail.com>
2022-03-15 13:19:13 +0300

    1change

---
+++ completehtml - Copy/javascriptmagic.js
@@ -0,0 +1,4 @@
+document.querySelector('.scroll-btn').addEventListener('click',()=>{document.querySelector('html').style.scrollBehavior='smooth';
+setTimeout(()=>{document.querySelector('html').style.scrollBehavior='unset';},1000)
+})
+
